8b72a6fc35bd2593b6e4a6dd96db2af65726d53a
shengxiaojie <shengxiaojie@shwoody.com>
2023-10-25 14:51:50 +0800

    address/cart

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { createSSRApp } from 'vue'
 import pinia from './stores'
 
 import App from './App.vue'
+
+// import Vconsole from 'vconsole'
+/*
+// #ifndef APP-PLUS
+const vConsole = new Vconsole()
+// #endif
+*/
+
 export function createApp() {
   const app = createSSRApp(App)
-
   app.use(pinia)
   return {
     app,
